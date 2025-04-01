43344edd4a70cb2751dc24cc7b9754658421dc8b
Jo3-h <joe.hosking1@gmail.com>
2025-04-01 14:59:48 +1030

    added logging middleware

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,22 @@
 
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const logger = require('./config/logger');
+
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(
+    morgan("combined", {stream: {write: (msg) => logger.info(msg.trim())}})
+);
+app.use((err, req, res, next) => {
+    logger.error(`Error occurred: ${err.message}`);
+    res.status(500).send("Internal Server Error");
+  });
 
 // Import Routes
 const githubRoutes = require('routes/githubRoutes');
