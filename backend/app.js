d44d1d00cb57dfd9195828037e7e756ac53e580d
Jo3-h <joe.hosking1@gmail.com>
2025-04-03 12:14:08 +1030

    configured route files to application in app.js file

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 // /app.js
 
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const logger = require('./config/logger');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const logger = require("./config/logger");
 
 const app = express();
 
@@ -12,22 +12,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(
-    morgan("combined", {stream: {write: (msg) => logger.info(msg.trim())}})
+  morgan("combined", { stream: { write: (msg) => logger.info(msg.trim()) } })
 );
 app.use((err, req, res, next) => {
-    logger.error(`Error occurred: ${err.message}`);
-    res.status(500).send("Internal Server Error");
-  });
+  logger.error(`Error occurred: ${err.message}`);
+  res.status(500).send("Internal Server Error");
+});
 
 // Import Routes
-const githubRoutes = require('routes/githubRoutes');
-const leetcodeRoutes = require('routes/leetcodeRoutes');
-const stravaRoutes = require('routes/stravaRoutes');
+const githubRoutes = require("routes/githubRoutes");
+const leetcodeRoutes = require("routes/leetcodeRoutes");
+const stravaRoutes = require("routes/stravaRoutes");
+const resumeRoutes = require("routes/resumeRoutes");
 
 // Use Routes
-app.use('/api/github', githubRoutes);
-app.use('/api/leetcode', leetcodeRoutes);
-app.use('/api/strava', stravaRoutes);
+app.use("/api/github", githubRoutes);
+app.use("/api/leetcode", leetcodeRoutes);
+app.use("/api/strava", stravaRoutes);
+app.use("/api/resume", resumeRoutes);
 
 // Export the app
 module.exports = app;
