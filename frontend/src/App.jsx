25a0aaf125c67aa3d48ff02b9c7da17738e86489
Jo3-h <joe.hosking1@gmail.com>
2025-04-07 16:38:35 +0930

    update from 17/04

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,26 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-blue-300">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="w-screen h-auto min-h-full bg-blue-300 absolute top-0 left-0"></div>
     </>
   );
 }
