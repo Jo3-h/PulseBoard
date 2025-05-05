a203a62fa36cd8c94f16279fc561bdefceb790a3
Jo3-h <joe.hosking1@gmail.com>
2025-05-05 18:03:14 +0930

    altered server entrypoint to attempt to read and use SSL certificate

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,38 @@
 
 require("dotenv").config();
 const app = require("./app");
+const https = require("https");
+const fs = require("fs");
 const PORT = process.env.APP_PORT || 5000;
 
-// Start the app
-app.listen(PORT, () => {
-  console.log(`PulseBoard API Server running on port ${PORT}`);
-});
+// try building the server with https otherwise run with http
+try {
+  // Read SSL certificate files
+  const privateKey = fs.readFileSync(
+    "/etc/letsencrypt/live/pulseboardapp.com/privkey.pem",
+    "utf8"
+  );
+  const certificate = fs.readFileSync(
+    "/etc/letsencrypt/live/pulseboardapp.com/cert.pem",
+    "utf8"
+  );
+  const ca = fs.readFileSync(
+    "/etc/letsencrypt/live/pulseboardapp.com/chain.pem",
+    "utf8"
+  );
+
+  // create HTTPS server
+  const credentials = { key: privateKey, cert: certificate, ca: ca };
+
+  const server = https.createServer(credentials, app);
+
+  // Start the app
+  server.listen(PORT, () => {
+    console.log(`PulseBoard HTTPs API Server running on port ${PORT}`);
+  });
+} catch (error) {
+  console.warn("\x1b[31m%s\x1b[0m", "Failed to read SSL certificate");
+  app.listen(PORT, () => {
+    console.log(`PulseBoard HTTP API Server running on port ${PORT}`);
+  });
+}
